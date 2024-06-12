cc856049e07c77fc8b865b6b415e917e14efc48c
joe-newm <j.newm500@gmail.com>
2024-06-11 20:27:52 -0500

    created boiler plate for pyside app, window opens

--- main.py
+++ main.py
@@ -2,3 +2,18 @@ import sys
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
 from PySide6.QtCore import Qt
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Joseph's Pixel Art Editor")
+        self.resize(1000,1000)
+
+
+###################### Main #####################
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+sys.exit(app.exec())
+
